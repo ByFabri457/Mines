831f36327cea13d3825f7d301fff589e252d4bc1
ByFabri457 <fabrilaso04@gmail.com>
2023-01-18 15:59:40 +0100

    Distribuir Metodes

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,81 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    public static int[][] taula;
+    static int midaTaulaX;
+    static int midaTaulaY;
     public static void main(String[] args) {
-        int posicioY = 0;
-        int posicioX = 0;
-        int[][] taula = new int[8][8];
-        Random rand = new Random();
-        int numeroMines = 5;
-        int[] cordenadesMinesY = new int[numeroMines];
-        int[] cordenadesMinesX = new int[numeroMines];
-        for (int i = 0;i < numeroMines; i++) {
-            posicioX = rand.nextInt(1, 7);
-            posicioY = rand.nextInt(1, 7);
-            cordenadesMinesX[i] = posicioX;
-            cordenadesMinesY[i] = posicioY;
-            for (int j = 0;j < i;j++) {
 
-                if (cordenadesMinesX[i] == cordenadesMinesX[j] && cordenadesMinesY[i] == cordenadesMinesY[j]) {
-                    --i;
-                }
-            }
-            System.out.println(posicioX+" - "+posicioY+", ");
+        crearTaula();
 
-        }
-        for (int i = 0; i < 8; i++) {
+        afegirMines();
 
-            for (int j = 0; j < 8; j++) {
-                for (int k = 0;k<numeroMines;k++) {
-                    if (i == cordenadesMinesX[k] && j == cordenadesMinesY[k]) {
-                        taula[i][j] = 3;
-                        break;
-                    } else {
-                        taula[i][j] = 0;
-                    }
-                }
-            }
-        }
-        for (int i = 0;i < 8;i++) {
-            for (int j = 0;j < 8;j++) {
+        for (int i = 0; i < midaTaulaX; i++) {
+            for (int j = 0; j < midaTaulaY; j++) {
                 System.out.print(taula[i][j] + "  ");
             }
             System.out.println();
         }
 
+    }
+
+    public static void crearTaula() {
+
+        System.out.println("Escolleigx la mida de la taula");
+        System.out.print("Numero Files:");
+        midaTaulaX = sc.nextInt();
+        System.out.println();
+        System.out.print("Numero Columnes:");
+        midaTaulaY = sc.nextInt();
+        taula = new int[midaTaulaX][midaTaulaY];
+    }
+    public static void afegirMines() {
+        int posicioY = 0;
+        int posicioX = 0;
+        boolean minaTrobada;
+
+
+
+        Random rand = new Random();
+
+        System.out.println("Escriu el numero de mines");
+
+        int numeroMines = sc.nextInt();
+
+        int[] cordenadesMinesY = new int[numeroMines];
+        int[] cordenadesMinesX = new int[numeroMines];
+
+        for (int i = 0; i < numeroMines; i++) {
+            posicioX = rand.nextInt(0, midaTaulaX);
+            posicioY = rand.nextInt(0, midaTaulaY);
+            cordenadesMinesX[i] = posicioX;
+            cordenadesMinesY[i] = posicioY;
+            for (int j = 0; j < i; j++) {
+
+                if (cordenadesMinesX[i] == cordenadesMinesX[j] && cordenadesMinesY[i] == cordenadesMinesY[j]) {
+                    --i;
+                }
+            }
+            System.out.println(posicioX+" - "+posicioY);
+
+        }
+        for (int i = 0; i < midaTaulaX; i++) {
+
+            for (int j = 0; j < midaTaulaY; j++) {
+                minaTrobada = false;
+                for (int k = 0; k < numeroMines; k++) {
+
+                    if (i == cordenadesMinesX[k] && j == cordenadesMinesY[k]) {
+                        taula[i][j] = 3;
+                        minaTrobada = true;
+                    } else if (minaTrobada == false) {
+                        taula[i][j] = 0;
+                    }
+                }
+            }
+        }
     }
 }
