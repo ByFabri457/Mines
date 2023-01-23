7212627b1bc5dbd0378b32585340ebe0bdd6ee6b
arnau <arnaugibert@inspladelestany.cat>
2023-01-23 15:31:02 +0100

    Game Over

--- src/Main.java
+++ src/Main.java
@@ -37,14 +37,14 @@ class Main {
                 TaulaUsuari.mostrarTaulaUsuari(taula);
                 System.out.println();
                 do {
-                    System.out.println("Escogeix fila (1-" + taula.midaTaulaX + "):");
+                    System.out.println("Escolleix fila (1-" + taula.midaTaulaX + "):");
                     modificarX = sc.nextInt();
                     if (modificarX > taula.midaTaulaX + 1 || modificarX < 1) {
                         System.out.println("No s'ha trobat la columna " + modificarX);
                     }
                 } while (modificarX > taula.midaTaulaX + 1 || modificarX < 1);
                 do {
-                    System.out.println("Escogeix columna (1-" + taula.midaTaulaY + "):");
+                    System.out.println("Escolleix columna (1-" + taula.midaTaulaY + "):");
                     modificarY = sc.nextInt();
                     if (modificarY > taula.midaTaulaY + 1 || modificarY < 1) {
                         System.out.println("No s'ha trobat la fila " + modificarY);
@@ -56,7 +56,26 @@ class Main {
             }
             if (taula.gameOver) {
                 TaulaUsuari.mostrarTaulaUsuari(taula);
-                System.out.println("Has Perdut!");
+                System.out.println("┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀\n" +
+                        "██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀\n" +
+                        "███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼\n" +
+                        "██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼\n" +
+                        "███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼████▄┼┼┼▄▄▄▄▄▄▄┼┼┼▄████┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼▀▀█▄█████████▄█▀▀┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼██▀▀▀███▀▀▀██┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼██┼┼┼███┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼█████▀▄▀█████┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼▄▄▄██┼┼█▀█▀█┼┼██▄▄▄┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼▀▀██┼┼┼┼┼┼┼┼┼┼┼██▀▀┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n");
+
             }
         }
     }
@@ -64,14 +83,13 @@ class Main {
     public static void mostrarMenu() {
         System.out.println("|B_U_S_C_A_M_I_N_E_S|");
         System.out.println();
-        System.out.println("-Fet per Fabricio Laso, Arnau Gibert i Xaiver Pandele");
+        System.out.println("-Fet per Fabricio Laso, Arnau Gibert i Xavier Pandele");
         System.out.println();
-        System.out.println("Escogeix opcio:");
+        System.out.println("Selecciona una opcio:");
         System.out.println("-------------------------------");
         System.out.println("1-Començar una nova partida");
-        System.out.println("2-Mirar llibre de records (NO DISPONIBLE)");
+        System.out.println("2-Mirar llibre de récords (NO DISPONIBLE)");
         System.out.println("3-Canviar disseny del taulell (NO DISPONIBLE)");
         System.out.println("0-Sortir");
     }
-
 }
