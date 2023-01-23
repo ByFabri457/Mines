8ad32eee34449f7dd026ea4345f2badb96df1939
arnau <arnaugibert@inspladelestany.cat>
2023-01-23 15:31:50 +0100

    Correccio de codi

--- src/TaulaVirtual.java
+++ src/TaulaVirtual.java
@@ -6,31 +6,31 @@ public class TaulaVirtual {
     public static boolean gameOver = false;
     public static int[][] taula;
 
-    public static int[] indexExploracioX = {-1,0,+1,+1,+1,0,-1,-1};
+    public static int[] indexExploracioX = {-1, 0, +1, +1, +1, 0, -1, -1};
 
-    public static int[] indexExploracioY = {-1,-1,-1,0,+1,+1,+1,0};
+    public static int[] indexExploracioY = {-1, -1, -1, 0, +1, +1, +1, 0};
     public static int midaTaulaX;
     public static int midaTaulaY;
+
     public static void crearTaula() {
 
-        System.out.println("Escolleigx la mida de la taula");
-        System.out.print("Numero Files:");
+        System.out.println("Escolleix la mida de la taula");
+        System.out.print("Número Files:");
         midaTaulaX = sc.nextInt();
         System.out.println();
-        System.out.print("Numero Columnes:");
+        System.out.print("Número Columnes:");
         midaTaulaY = sc.nextInt();
-        taula = new int[midaTaulaX+2][midaTaulaY+2];
+        taula = new int[midaTaulaX + 2][midaTaulaY + 2];
     }
+
     public static void crearMines() {
         int posicioY = 0;
         int posicioX = 0;
 
 
-
-
         Random rand = new Random();
 
-        System.out.println("Escriu el numero de mines");
+        System.out.println("Escriu el número de mines");
 
         int numeroMines = sc.nextInt();
 
@@ -38,8 +38,8 @@ public class TaulaVirtual {
         int[] cordenadesMinesX = new int[numeroMines];
 
         for (int i = 0; i < numeroMines; i++) {
-            posicioX = rand.nextInt(1, midaTaulaX+1);
-            posicioY = rand.nextInt(1, midaTaulaY+1);
+            posicioX = rand.nextInt(1, midaTaulaX + 1);
+            posicioY = rand.nextInt(1, midaTaulaY + 1);
             cordenadesMinesX[i] = posicioX;
             cordenadesMinesY[i] = posicioY;
             for (int j = 0; j < i; j++) {
@@ -48,29 +48,27 @@ public class TaulaVirtual {
                     --i;
                 }
             }
-
-
         }
-        colocarMines(numeroMines,cordenadesMinesX,cordenadesMinesY);
-
+        colocarMines(numeroMines, cordenadesMinesX, cordenadesMinesY);
     }
-    public static void modificarTaula(int eleccioX,int eleccioY) {
+
+    public static void modificarTaula(int eleccioX, int eleccioY) {
         int indexMina;
-        for (int i = 1;i < midaTaulaX+1;i++) {
-            for (int j = 1;j < midaTaulaY+1;j++) {
+        for (int i = 1; i < midaTaulaX + 1; i++) {
+            for (int j = 1; j < midaTaulaY + 1; j++) {
                 if (i == eleccioX && j == eleccioY) {
                     if (taula[i][j] == 0) {
                         indexMina = 0;
-                        for (int k = 0;k < 8;k++) {
+                        for (int k = 0; k < 8; k++) {
 
-                            if (taula[i+indexExploracioX[k]][j+indexExploracioY[k]] == 3) {
+                            if (taula[i + indexExploracioX[k]][j + indexExploracioY[k]] == 3) {
                                 indexMina += 1;
                             }
 
                         }
                         if (indexMina == 0) {
                             taula[i][j] = 1;
-                            expandirTaulell(eleccioX,eleccioY);
+                            expandirTaulell(eleccioX, eleccioY);
                         }
                         if (indexMina >= 1) taula[i][j] = indexMina + 10;
                     }
@@ -81,11 +79,12 @@ public class TaulaVirtual {
             }
         }
     }
-    public static void colocarMines(int numeroMines,int[] cordenadesMinesX,int[] cordenadesMinesY) {
-        boolean minaTrobada;
-        for (int i = 1; i < midaTaulaX+1; i++) {
 
-            for (int j = 1; j < midaTaulaY+1; j++) {
+    public static void colocarMines(int numeroMines, int[] cordenadesMinesX, int[] cordenadesMinesY) {
+        boolean minaTrobada;
+        for (int i = 1; i < midaTaulaX + 1; i++) {
+
+            for (int j = 1; j < midaTaulaY + 1; j++) {
                 minaTrobada = false;
                 for (int k = 0; k < numeroMines; k++) {
 
@@ -100,9 +99,9 @@ public class TaulaVirtual {
         }
     }
 
-    public static void expandirTaulell(int eleccioX,int eleccioY) {
-        for (int i = 0;i < 8; i++) {
-            modificarTaula(eleccioX+indexExploracioX[i],eleccioY+indexExploracioY[i]);
+    public static void expandirTaulell(int eleccioX, int eleccioY) {
+        for (int i = 0; i < 8; i++) {
+            modificarTaula(eleccioX + indexExploracioX[i], eleccioY + indexExploracioY[i]);
         }
     }
 }
